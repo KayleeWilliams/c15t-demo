c65f3df15c5d8015aa63ecab79a62254d742ce22
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2025-10-22 11:24:32 +0100

    run formatter

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,8 +1,8 @@
-import posthog from "posthog-js";
+import posthog from 'posthog-js';
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: "2025-05-24"
-  // PostHog will not set any cookies until the user has given consent.
-  // cookieless_mode: "on_reject"
+	api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+	defaults: '2025-05-24',
+	// PostHog will not set any cookies until the user has given consent.
+	// cookieless_mode: "on_reject"
 });
