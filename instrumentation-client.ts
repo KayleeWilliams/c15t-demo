ba94cd8c9210e87ef43f0b5c7a0c1b6c7f7239a2
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2025-10-22 09:29:46 +0100

    add posthog

---
+++ instrumentation-client.ts
@@ -0,0 +1,8 @@
+import posthog from "posthog-js";
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24"
+  // PostHog will not set any cookies until the user has given consent.
+  // cookieless_mode: "on_reject"
+});
